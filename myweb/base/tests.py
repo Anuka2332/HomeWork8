79852c79f3984d54a830088e7e2bca2bcc4f0a5a
Ana Endeladze <anuka2332@gmail.com>
2024-07-26 18:50:21 +0400

    HomeWork155

--- myweb/base/tests.py
+++ myweb/base/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import Animal
 # Create your tests here.
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Animal.objects.create(name='Dog', sound="woof")
+        Animal.objects.create(name='Wolf', sound="owoooo")
+
+    def test_animal_can_speak(self):
+        wolf = Animal.objects.get(name='Wolf')
+        dog = Animal.objects.get(name='Dog')
+        self.assertEqual(wolf.speak(), 'The Wolf says "owoooo"')
+        self.assertEqual(dog.speak(), 'The Dog says "woof"')
+
+
